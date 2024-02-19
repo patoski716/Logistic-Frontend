1aeeafe36d30fc19a00b464eaa7f3026b347dc37
ptoski716 <patoski716@gmail.com>
2024-02-19 01:07:23 +0100

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     AOS.init({
       // Global settings for AOS (optional)
-      duration: 2000,
+      duration: 3000,
       once: true, // Whether animation should happen only once while scrolling down
     });
   }, []); // Ensure this useEffect runs only once after component mount
